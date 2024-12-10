621bf88ab2f68873aac6c84b1d2887f9eabe8405
adeliya21 <adeliya.salieva@gmail.com>
2024-12-09 20:38:21 -0800

    added synchronous hashing and comparing hashes

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ app.get('/', (req, res) => {
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-    console.log(hash);
+    console.log(hash)
     //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
     bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
-      console.log(res); //true
+      console.log(res) //true
     });
 });
 
@@ -27,9 +27,11 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 //END_ASYNC
 
 //START_SYNC
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds)
+console.log(hash)
 
-
-
+var res = bcrypt.compareSync(myPlaintextPassword, hash)
+console.log(res)
 //END_SYNC
 
 
